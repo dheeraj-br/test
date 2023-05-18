0e859e56a2437c32ae92a0a13dee07f069ff7bc3
dheeraj <dheeraj.br2@gmail.com>
2023-05-18 18:13:23 +0530

    Setup project: facade and interfaces with TS

---
+++ src/Company.ts
@@ -0,0 +1,22 @@
+import { faker } from '@faker-js/faker';
+import { Mappable } from './CustomMap';
+
+export class Company implements Mappable {
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  constructor() {
+    this.companyName = faker.company.name();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: faker.location.latitude(),
+      lng: faker.location.longitude(),
+    };
+  }
+  getMarkerContent(): string {
+    return this.companyName;
+  }
+}
---
+++ src/User.ts
@@ -0,0 +1,20 @@
+import { faker } from '@faker-js/faker';
+import { Mappable } from './CustomMap';
+
+export class User implements Mappable {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  constructor() {
+    this.name = faker.person.fullName();
+    this.location = {
+      lat: faker.location.latitude(),
+      lng: faker.location.longitude(),
+    };
+  }
+  getMarkerContent(): string {
+    return this.name;
+  }
+}
